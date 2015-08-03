2bcee1222ccee9f15d83736fc823678ec68b47f1
dolgarev <o.dolgarev@gmail.com>
2015-08-03 09:43:28 +0300

    added "reactive" param

--- inc_and_dec.js
+++ inc_and_dec.js
@@ -1,13 +1,15 @@
 if (!(ReactiveVar.prototype.inc && ReactiveVar.prototype.inc.constructor === Function)) {
-  ReactiveVar.prototype.inc = function(step) {
+  ReactiveVar.prototype.inc = function(step, reactive) {
     step = step === void 0 ? 1 : (step ^ (step >> 31)) - (step >> 31);
-    return this.set(this.get() + step);
+    var val = reactive ? this.get() : this.curValue;
+    return this.set(val + step);
   };
 }
 
 if (!(ReactiveVar.prototype.dec && ReactiveVar.prototype.dec.constructor === Function)) {
-  ReactiveVar.prototype.dec = function(step) {
+  ReactiveVar.prototype.dec = function(step, reactive) {
     step = step === void 0 ? 1 : (step ^ (step >> 31)) - (step >> 31);
-    return this.set(this.get() - step);
+    var val = reactive ? this.get() : this.curValue;
+    return this.set(val - step);
   };
 }
